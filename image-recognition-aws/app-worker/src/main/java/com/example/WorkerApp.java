dd762d998aa69f3c5c85e02385b71547dafa2a84
Vamshi Maya <vamshi.maya005@gmail.com>
2025-05-14 13:11:49 -0500

    fix: correct SQS polling and Iterable error in WorkerApp, ensure AWS SDK dependencies are correct

--- image-recognition-aws/app-worker/src/main/java/com/example/WorkerApp.java
+++ image-recognition-aws/app-worker/src/main/java/com/example/WorkerApp.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +11,11 @@ import software.amazon.awssdk.services.rekognition.model.Image;
 import software.amazon.awssdk.services.s3.S3Client;
 import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class WorkerApp {
@@ -24,14 +28,32 @@ public class WorkerApp {
 class SqsImageHandler {
   private final S3Client s3;
   private final RekognitionClient rekognition;
+  private final SqsClient sqs;
   @Value("${app.bucket}") String bucket;
+  @Value("${sqs.queue.url}") String queueUrl;
 
-  public SqsImageHandler(S3Client s3, RekognitionClient rekognition) {
+  @Autowired
+  public SqsImageHandler(S3Client s3, RekognitionClient rekognition, SqsClient sqs) {
     this.s3 = s3;
     this.rekognition = rekognition;
+    this.sqs = sqs;
   }
 
-  @SqsListener("${sqs.queue.url}")
+  // Poll SQS every 5 seconds
+  @Scheduled(fixedDelay = 5000)
+  public void pollSqs() {
+    java.util.List<Message> messages = sqs.receiveMessage(r -> r.queueUrl(queueUrl).maxNumberOfMessages(5)).messages();
+    for (Message msg : messages) {
+      try {
+        handle(msg.body());
+        sqs.deleteMessage(r -> r.queueUrl(queueUrl).receiptHandle(msg.receiptHandle()));
+      } catch (Exception e) {
+        // Log and continue
+        e.printStackTrace();
+      }
+    }
+  }
+
   void handle(String key) {
       InputStream img = s3.getObject(b -> b.bucket(bucket).key(key));
       var labels = rekognition.detectLabels(r -> r.image(

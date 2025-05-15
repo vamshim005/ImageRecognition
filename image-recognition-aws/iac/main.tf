9b90f386fd236ab1eb892d87022ba8b52bde63e3
Vamshi Maya <vamshi.maya005@gmail.com>
2025-05-14 23:18:55 -0500

    fix: update ALB and ECS cluster configuration to resolve Terraform errors

--- image-recognition-aws/iac/main.tf
+++ image-recognition-aws/iac/main.tf
@@ -11,6 +11,26 @@ module "vpc" {
 module "cluster" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "5.8.0"
+  
+  cluster_name = "imgrec-cluster"
+  
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/imgrec-cluster"
+      }
+    }
+  }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 100
+        base   = 1
+      }
+    }
+  }
 }
 
 resource "aws_sqs_queue" "jobs" {
@@ -56,19 +76,56 @@ module "alb" {
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
   enable_http2       = true
+
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
   listeners = {
     https = {
       port            = 443
       protocol        = "HTTPS"
       certificate_arn = var.acm_cert_arn
-      target_groups = [
-        {
-          name_prefix      = "web-"
-          backend_port     = 8080
-          backend_protocol = "HTTP"
-          target_type      = "ip"
-        }
-      ]
+      forward = {
+        target_group_key = "web"
+      }
+    }
+  }
+
+  target_groups = {
+    web = {
+      name_prefix          = "web-"
+      backend_protocol     = "HTTP"
+      backend_port         = 8080
+      target_type          = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
     }
   }
 }
@@ -171,7 +228,7 @@ module "web" {
   cluster_id        = module.cluster.cluster_id
   subnets           = module.vpc.private_subnets
   security_groups   = ["${aws_security_group.web.id}"]
-  target_group_arn  = module.alb.target_groups["https-web-0"].arn
+  target_group_arn  = module.alb.target_groups["web"].arn
   listener_port     = 8080
   execution_role_arn = module.web_task_role.arn
   task_role_arn      = module.web_task_role.arn
